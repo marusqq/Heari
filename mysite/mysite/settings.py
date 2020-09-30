95f3f77143b36c82e7cac6cffb1cce5f83f3cec1
Marius Pozniakovas <pozniakovui@gmail.com>
2020-09-30 10:35:01 +0300

    moving to pythonanywhere

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -23,9 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '@0pzyp49%+ufj#7t77iz^1w6yc4l9v73kn#!qnexayq9#$2q+-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+#ALLOWED_HOSTS = ['127.0.0.1', '192.168.1.104']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
